e888c88060991cd32ba3a6b784c0efc7a2f3a41b
diego <di.bolivar@duocuc.cl>
2026-04-14 23:35:29 -0400

    SE AGREGO EL SPRING BOOT

---
+++ src/main/java/com/duoc/Scoutball/ScoutballApplication.java
@@ -0,0 +1,13 @@
+package com.duoc.Scoutball;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScoutballApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScoutballApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/duoc/Scoutball/ScoutballApplicationTests.java
@@ -0,0 +1,13 @@
+package com.duoc.Scoutball;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScoutballApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
