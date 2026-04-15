b5ae745ffe9a70405c3506c6ad5db669105ecc29
diego <di.bolivar@duocuc.cl>
2026-04-15 13:02:53 -0400

    creacion model y coneccion con sql

---
+++ src/main/java/com/duoc/Scoutball/model/equipos.java
@@ -0,0 +1,49 @@
+package com.duoc.Scoutball.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="equipos")
+public class equipos {
+     @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String nombre_equipo;
+
+    @NotBlank
+    private String ubicacion;
+
+    @NotBlank
+    private String pais;
+
+    @NotBlank
+    private String liga;
+
+    @NotBlank
+    private String tecnico;
+
+    private Integer titulos;
+
+    @NotBlank
+    private String correo;
+
+    @NotBlank
+    private String numero_tlf;
+
+    private String  interes;
+
+
+}
---
+++ src/main/java/com/duoc/Scoutball/model/jugadores.java
@@ -0,0 +1,76 @@
+package com.duoc.Scoutball.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name="jugadores")
+
+public class jugadores {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    @NotBlank
+    private String pnombre;
+
+    @NotBlank
+    private String snombre;
+
+    @NotBlank
+    private String appaterno;
+
+    @NotBlank
+    private String apmaterno;
+
+    @NotBlank
+    private String nacionalidad;
+
+    @NotNull
+    private Integer fecha_nacimiento;
+
+    @NotBlank
+    private String correo;
+
+    @NotBlank
+    private String numero_tlf;
+
+    @NotBlank
+    private String direccion;
+
+    @NotBlank
+    private String  sobre_mi;
+
+    @NotBlank
+    private String posicion;
+
+    @NotBlank
+    private float altura;
+
+    @NotBlank
+    private Float peso;
+
+    @NotBlank
+    private String pierna_habil;
+
+    private String representante;
+
+    private String equipo;
+
+
+
+
+
+
+}
