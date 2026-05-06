cc2cafe75746c1ed6fb6e3cdb0c573dc5ed5516b
diego <di.bolivar@duocuc.cl>
2026-05-06 13:40:40 -0400

    se agrega cosas al pom y se solucionan errores

--- src/main/java/com/duoc/Scoutball/service/WeatherService.java
+++ src/main/java/com/duoc/Scoutball/service/WeatherService.java
@@ -3,6 +3,7 @@ package com.duoc.Scoutball.service;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import com.duoc.Scoutball.dto.WeatherDTO;
 
@@ -14,7 +15,7 @@ public class WeatherService {
     private WebClient weatherWebClient;
 
      public WeatherDTO obtenerClima(double latitude, double longitude) {
-        return weatherWebClient.get()
+        return  weatherWebClient.get()
                 .uri(uriBuilder -> uriBuilder
                         .path("/v1/forecast")
                         .queryParam("latitude", latitude)
