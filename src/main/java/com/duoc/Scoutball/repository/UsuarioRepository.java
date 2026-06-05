a1bb82d0970ee5ce9e4151e0569d3d9b76c8cb99
karla <kar.mirandac@duocuc.cl>
2026-06-05 13:49:00 -0400

    se añade spring security

---
+++ src/main/java/com/duoc/Scoutball/repository/UsuarioRepository.java
@@ -0,0 +1,12 @@
+package com.duoc.Scoutball.repository;
+
+import com.duoc.Scoutball.model.Usuario;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface UsuarioRepository extends JpaRepository<Usuario, Integer> {
+    Optional<Usuario> findByUsername(String username);
+}
