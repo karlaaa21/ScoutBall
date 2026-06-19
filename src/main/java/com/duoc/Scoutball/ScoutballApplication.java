7d730929e726c0082b8f3fba6d01df92a6776fe0
diego <di.bolivar@duocuc.cl>
2026-06-19 19:39:48 -0400

    se actualiza scoutballapplication

--- src/main/java/com/duoc/Scoutball/ScoutballApplication.java
+++ src/main/java/com/duoc/Scoutball/ScoutballApplication.java
@@ -1,13 +1,31 @@
 package com.duoc.Scoutball;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ScoutballApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(ScoutballApplication.class, args);
-	}
+@SpringBootApplication
+@OpenAPIDefinition(
+  info = @Info(
+    title = "Scoutball API",
+    version = "1.0"
+  ),
+  security = @SecurityRequirement(name = "Bearer Authentication")
+)
+@SecurityScheme(
+  name = "Bearer Authentication",
+  type = SecuritySchemeType.HTTP,
+  scheme = "bearer",
+  bearerFormat = "JWT"
+)
+public class ScoutballApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(ScoutballApplication.class, args);
+  }
 
 }
